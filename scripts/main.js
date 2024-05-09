73862b03fe86499688b3e06384254462383fd839
edzordzinam44 <edzordzinam.alipui@gmail.com>
2024-05-09 03:16:24 +0000

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,6 @@
 // const myHeading = document.querySelector("h1");
 // myHeading.textContent = "Welcome to my Page!";
 
-const myImage = document.querySelector("img");
-myImage.onclick = () => {
-    const mySrc = myImage.getAttribute("src");
-    if (mySrc === "logo/icon_tv.png") {
-        myImage.setAttribute("src", "logo/icon_tv.png");
-    } else {
-        myImage.setAttribute("src", "logo/icon_tv.png")
-    }
-};
 
 let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
@@ -31,6 +22,6 @@ function setUserName() {
     setUserName()
   } else {
     const storedName = localStorage.getItem("name");
-    myHeading.textContent = `You're so Cool, ${myName}!!`;
+    myHeading.textContent = `Lets start the Journey, ${myName}!!`;
   }
 }
